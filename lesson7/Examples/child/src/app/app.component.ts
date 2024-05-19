e860de4f7ab4875d8380777e50e006dbfc7b0bb8
Filippov-Nikolay <filippovnikolay195@gmail.com>
2024-05-20 01:35:47 +0300

    hw8

--- lesson7/Examples/child/src/app/app.component.ts
+++ lesson7/Examples/child/src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChildComponent } from './child/child.component';
 import { FormsModule } from '@angular/forms';
-import { Child2Component } from './child2/child2.component';
-import { OutputComponent } from './output/output.component';
-import { Bind2Component } from './bind2/bind2.component';
+// import { Child2Component } from './child2/child2.component';
+// import { OutputComponent } from './output/output.component';
+// import { Bind2Component } from './bind2/bind2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ChildComponent,FormsModule,Child2Component,OutputComponent,Bind2Component],
+  imports: [RouterOutlet, ChildComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
